bf59704b93ba3d0fb7c501f78cabffdd93042689
EltarrLok <eltarr@ensta.fr>
2020-02-11 00:43:10 +0100

    stepwise , on obtient 20 variables

--- analyse.R
+++ analyse.R
@@ -2,15 +2,27 @@ rm(list = objects())
 graphics.off()
 setwd("/home/lokmen/Documents/ENSTA/SIM202/building-appliances/")   # set working directory
 
-library(readr)
 
-data <- read_delim("train.csv", col_names = TRUE, delim = ",")
-data_test<- read_delim("test.csv", col_names = TRUE, delim = ",")
+source("rmse.R")
+library(readr)
+library(tidyverse)
+library(lubridate)
+library(ranger)
+
+#data <- read_delim("train.csv", col_names = TRUE, delim = ",")
+#data_test<- read_delim("test.csv", col_names = TRUE, delim = ",")
+data0<-read.csv(file="train.csv", sep=",", dec='.')
+data_test<-read.csv(file="test.csv", sep=",", dec='.')
+
 # Pas besoin de mettre une date car il y en a deja une
 # Tracer variables en fonction d'une autre, voir correlation etc
 
 
-
+################################
+##############################
+############" Methode variable bien correllées
+###################################
+####################################
 
 Applience = data$Appliances#[1:7000]
 summary(data)
@@ -37,6 +49,104 @@ dim(submit)
 head(submit)
 submit$Appliances <- step.model.test
 str(submit)
+
+write.table(submit, file="submission_lm.csv", quote=F, sep=",", dec='.',row.names = F)
+
+######################################
+######################################
+###### Methode stepwise
+#####################################""
+#########################################
+
+cov <- head(names(data0)[-c(1,2)], 30) 
+n <- nrow(data0)
+set.seed(100)
+s <- sample(c(1:n), size=floor(n*0.5))
+length(s)
+
+l="Appliances ~"
+eq_list =list()
+eq_list[[1]] <-  paste0(l, paste0(cov[1], collapse='+'))
+
+####### On génère une liste de formule
+for(i in c(1:length(cov)))
+{
+  eq_list[[i]] <-  paste0(l, paste0(cov[[i]], collapse='+'))
+  eq_list[[i]]
+}
+
+####### On génère une liste de regression linéaire
+fitMod <- function(eq, subset)
+{
+  reg <- lm(eq, data=data0[-subset,])
+  return(reg)
+}
+reg_list <- lapply(eq_list, fitMod, subset=s)
+
+####### On prédit à partir de des regressions linéaires 
+reg_list_forecast <- lapply(reg_list, predict, newdata=data0[s,])
+
+rmse(reg_list_forecast[[1]],data0[s,]$Appliances)
+rmse_list <- lapply(reg_list_forecast, rmse, data0[s,]$Appliances)
+j = which.min(rmse_list) ## On repère l'indice qui minimise l'erreur
+### D'abord on vérifie qu'il y a un gain à rajouter la jème variable
+l
+if (rmse(reg_list_forecast[[j]],data0[s,]$Appliances) <score )
+{
+  score = rmse(reg_list_forecast[[j]],data0[s,]$Appliances)
+  l <- paste0(l, paste0(cov[j], collapse='+')) ## On rajoute le terme qui minimise à notre equation
+  l <-paste0(l, '+')
+}
+cov[-j]
+l
+
+
+####### Algorrithme Stepwise propre
+
+cov <- head(names(data0)[-c(1,2)], 30) 
+n <- nrow(data0)
+set.seed(100)
+s <- sample(c(1:n), size=floor(n*0.5))
+length(s)
+
+l="Appliances ~"
+eq_list =list()
+score = 1000
+for(i in c(2:length(cov)))
+{
+  for(k in c(1:length(cov)))
+  {
+    eq_list[[k]] <-  paste0(l, paste0(cov[[k]], collapse='+'))
+    eq_list[[k]]
+  }
+  reg_list <- lapply(eq_list, fitMod, subset=s)
+  reg_list_forecast <- lapply(reg_list, predict, newdata=data0[s,])
+  
+  rmse(reg_list_forecast[[1]],data0[s,]$Appliances)
+  rmse_list <- lapply(reg_list_forecast, rmse, data0[s,]$Appliances)
+  j = which.min(rmse_list) ## On repère l'indice qui minimise l'erreur
+  ### D'abord on vérifie qu'il y a un gain à rajouter la jème variable
+  l
+  if (rmse(reg_list_forecast[[j]],data0[s,]$Appliances) <score )
+  {
+    score = rmse(reg_list_forecast[[j]],data0[s,]$Appliances)
+    l <- paste0(l, paste0(cov[j], collapse='+')) ## On rajoute le terme qui minimise à notre equation
+    l <-paste0(l, '+')
+  }
+  cov[-j]
+
+}
+
+l<-strtrim(l,(nchar(l)-1))
+
+Appliance.lm = lm(l,data0)
+summary(Appliance.lm)
+step.model.test<-predict(Appliance.lm, data_test)
+which(is.na(step.model.test))
+
+
+submit <- read.csv(file="sample_submission.csv", sep=",", dec=".")
+submit$Appliances <- step.model.test
 
 write.table(submit, file="submission_lm.csv", quote=F, sep=",", dec='.',row.names = F)
 
@@ -90,14 +200,14 @@ corrplot(cor(df),method = "number",type="upper")
 #plot(data$date, data$T1-data$T2, type = 'h')
 
 #selectByDate(
-  data,
-  start = "1/1/2016",
-  end = "01/02/2016",
-  year = 2016,
-  month = 1,
-  day = "weekday",
-  hour = 1
-)
+#  data,
+#  start = "1/1/2016",
+#  end = "01/02/2016",
+#  year = 2016,
+#  month = 1,
+#  day = "weekday",
+#  hour = 1
+#)
 
 
 # CONSTRUIR UN MODELE DE REGRESSION LINEAIRE
